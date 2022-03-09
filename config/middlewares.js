f4a8e09e54802bb7157e67c4c168c9e1a0fce32a
LUIS RUANO <luisferuano@gmail.com>
2022-03-09 17:27:06 -0500

    fixes

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,15 +1,14 @@
 module.exports = [
+  "strapi::errors",
+  "strapi::security",
   {
     name: "strapi::cors",
     config: {
       enabled: true,
       header: "*",
-      origin: ["http://localhost:3000", "https://tu-voto-web-jeac.vercel.app"],
+      origin: "*",
     },
   },
-  "strapi::errors",
-  "strapi::security",
-  "strapi::cors",
   "strapi::poweredBy",
   "strapi::logger",
   "strapi::query",
